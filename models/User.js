837c19bd607cfefa60ec2d76d4bcfa486c3003ad
Eric Reyes <ericreyes1109@gmail.com>
2023-04-08 17:18:37 -0500

    added folders needed and created models

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true, 
+            trimmed: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true, 
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Must be a valid email address!'],
+        },
+        thoughts: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'thought',
+        }],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user'
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+   return this.friends.length;
+});
+
+const User = model( 'user', userSchema);
+
+model.exports = User;
